69a02a6eba52973980f985b99bcf20e43576a0e8
Civitaspo <civitaspo@gmail.com>
2020-01-26 14:15:22 +0900

    Generate data in test code for readability

--- src/test/scala/org/embulk/input/dynamodb/testutil/EmbulkTestBase.scala
+++ src/test/scala/org/embulk/input/dynamodb/testutil/EmbulkTestBase.scala
@@ -1,14 +1,42 @@
 package org.embulk.input.dynamodb.testutil
 
 
+import com.amazonaws.client.builder.AwsClientBuilder.EndpointConfiguration
+import com.amazonaws.services.dynamodbv2.{AmazonDynamoDB, AmazonDynamoDBClientBuilder}
 import org.embulk.input.dynamodb.DynamodbInputPlugin
 import org.embulk.spi.InputPlugin
 import org.embulk.test.TestingEmbulk
 import org.junit.{After, Rule}
 
+import scala.util.chaining._
+import scala.util.{Failure, Success, Try}
 
-class EmbulkTestBase
+trait EmbulkTestBase
 {
+    val dynamoDBHost: String = "localhost"
+    val dynamoDBPort: Int = 8000
+
+    def withDynamoDB[A](f: AmazonDynamoDB => A): A = {
+        AmazonDynamoDBClientBuilder.standard()
+            .withEndpointConfiguration(
+                new EndpointConfiguration(s"http://$dynamoDBHost:$dynamoDBPort", "us-east-1")
+                )
+            .build()
+            .pipe { client =>
+                try f(client)
+                finally client.shutdown()
+            }
+    }
+
+    def cleanupTable(name: String): Unit = {
+        withDynamoDB { dynamodb =>
+            Try(dynamodb.describeTable(name)) match {
+                case Success(_) => dynamodb.deleteTable(name)
+                case Failure(_) => // Do nothing.
+            }
+        }
+    }
+
     @Rule
     def embulk: TestingEmbulk = TestingEmbulk.builder()
         .registerPlugin(classOf[InputPlugin], "dynamodb", classOf[DynamodbInputPlugin])
