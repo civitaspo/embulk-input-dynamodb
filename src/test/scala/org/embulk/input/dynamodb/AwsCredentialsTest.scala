52f0c14c269e1c43d18b6d1f49758ae8fd7a2d83
Civitaspo <civitaspo@gmail.com>
2020-01-31 16:53:20 +0900

    Add new auth_method tests

--- src/test/scala/org/embulk/input/dynamodb/AwsCredentialsTest.scala
+++ src/test/scala/org/embulk/input/dynamodb/AwsCredentialsTest.scala
@@ -1,7 +1,8 @@
 package org.embulk.input.dynamodb
 
+import com.amazonaws.services.securitytoken.model.AWSSecurityTokenServiceException
 import org.embulk.config.{ConfigException, ConfigSource}
-import org.embulk.input.dynamodb.aws.{Aws, AwsCredentials}
+import org.embulk.input.dynamodb.aws.AwsCredentials
 import org.embulk.input.dynamodb.testutil.EmbulkTestBase
 import org.hamcrest.CoreMatchers._
 import org.hamcrest.MatcherAssert.assertThat
@@ -29,6 +30,11 @@ class AwsCredentialsTest extends EmbulkTestBase {
       "EMBULK_DYNAMODB_TEST_PROFILE_NAME"
     )
 
+  private lazy val EMBULK_DYNAMODB_TEST_ASSUME_ROLE_ROLE_ARN =
+    getEnvironmentVariableOrShowErrorMessage(
+      "EMBULK_DYNAMODB_TEST_ASSUME_ROLE_ROLE_ARN"
+    )
+
   def doTest(inConfig: ConfigSource): Unit = {
     val task: PluginTask = inConfig.loadConfig(classOf[PluginTask])
     val provider = AwsCredentials(task).createAwsCredentialsProvider
@@ -39,21 +45,44 @@ class AwsCredentialsTest extends EmbulkTestBase {
 
   def defaultInConfig: ConfigSource = {
     embulk.configLoader().fromYamlString(s"""
-         |type: dynamodb
-         |region: us-east-1
-         |table: hoge
-         |operation: scan
-         |columns:
-         |  - {name: key1,   type: string}
-         |  - {name: key2,   type: long}
-         |  - {name: value1, type: string}
-         |""".stripMargin)
+                                            |type: dynamodb
+                                            |region: us-east-1
+                                            |table: hoge
+                                            |operation: scan
+                                            |columns:
+                                            |  - {name: key1,   type: string}
+                                            |  - {name: key2,   type: long}
+                                            |  - {name: value1, type: string}
+                                            |""".stripMargin)
+  }
+
+  @deprecated(since = "0.3.0")
+  @Test
+  def notSetAuthMethod_SetCredentials_deprecated(): Unit = {
+    Assume.assumeTrue(runAwsCredentialsTest)
+    val inConfig: ConfigSource = defaultInConfig
+      .set("access_key", EMBULK_DYNAMODB_TEST_ACCESS_KEY)
+      .set("secret_key", EMBULK_DYNAMODB_TEST_SECRET_KEY)
+
+    doTest(inConfig)
   }
 
   @Test
   def notSetAuthMethod_SetCredentials(): Unit = {
     Assume.assumeTrue(runAwsCredentialsTest)
     val inConfig: ConfigSource = defaultInConfig
+      .set("access_key_id", EMBULK_DYNAMODB_TEST_ACCESS_KEY)
+      .set("secret_access_key", EMBULK_DYNAMODB_TEST_SECRET_KEY)
+
+    doTest(inConfig)
+  }
+
+  @deprecated(since = "0.3.0")
+  @Test
+  def setAuthMethod_Basic_deprecated(): Unit = {
+    Assume.assumeTrue(runAwsCredentialsTest)
+    val inConfig: ConfigSource = defaultInConfig
+      .set("auth_method", "basic")
       .set("access_key", EMBULK_DYNAMODB_TEST_ACCESS_KEY)
       .set("secret_key", EMBULK_DYNAMODB_TEST_SECRET_KEY)
 
@@ -65,8 +94,34 @@ class AwsCredentialsTest extends EmbulkTestBase {
     Assume.assumeTrue(runAwsCredentialsTest)
     val inConfig: ConfigSource = defaultInConfig
       .set("auth_method", "basic")
+      .set("access_key_id", EMBULK_DYNAMODB_TEST_ACCESS_KEY)
+      .set("secret_access_key", EMBULK_DYNAMODB_TEST_SECRET_KEY)
+
+    doTest(inConfig)
+  }
+
+  @deprecated(since = "0.3.0")
+  @Test(expected = classOf[ConfigException])
+  def throwIfSetAccessKeyAndAccessKeyId(): Unit = {
+    Assume.assumeTrue(runAwsCredentialsTest)
+    val inConfig: ConfigSource = defaultInConfig
+      .set("auth_method", "basic")
+      .set("access_key", EMBULK_DYNAMODB_TEST_ACCESS_KEY)
+      .set("access_key_id", EMBULK_DYNAMODB_TEST_ACCESS_KEY)
+      .set("secret_key", EMBULK_DYNAMODB_TEST_SECRET_KEY)
+
+    doTest(inConfig)
+  }
+
+  @deprecated(since = "0.3.0")
+  @Test(expected = classOf[ConfigException])
+  def throwIfSetSecretKeyAndSecretAccessKeyId(): Unit = {
+    Assume.assumeTrue(runAwsCredentialsTest)
+    val inConfig: ConfigSource = defaultInConfig
+      .set("auth_method", "basic")
       .set("access_key", EMBULK_DYNAMODB_TEST_ACCESS_KEY)
       .set("secret_key", EMBULK_DYNAMODB_TEST_SECRET_KEY)
+      .set("secret_access_key", EMBULK_DYNAMODB_TEST_SECRET_KEY)
 
     doTest(inConfig)
   }
@@ -105,6 +160,28 @@ class AwsCredentialsTest extends EmbulkTestBase {
     val inConfig: ConfigSource = defaultInConfig
       .set("auth_method", "profile")
       .set("profile_name", "DO_NOT_EXIST")
+
+    doTest(inConfig)
+  }
+
+  @Test
+  def setAuthMethod_assume_role(): Unit = {
+    Assume.assumeTrue(runAwsCredentialsTest)
+    val inConfig: ConfigSource = defaultInConfig
+      .set("auth_method", "assume_role")
+      .set("role_arn", EMBULK_DYNAMODB_TEST_ASSUME_ROLE_ROLE_ARN)
+      .set("role_session_name", "dummy")
+
+    doTest(inConfig)
+  }
+
+  @Test(expected = classOf[AWSSecurityTokenServiceException])
+  def setAuthMethod_assume_role_NotExistRoleArn(): Unit = {
+    Assume.assumeTrue(runAwsCredentialsTest)
+    val inConfig: ConfigSource = defaultInConfig
+      .set("auth_method", "assume_role")
+      .set("role_arn", "DO_NOT_EXIST")
+      .set("role_session_name", "dummy")
 
     doTest(inConfig)
   }
