750744202d9651b4c4f9e19c3072862dc0abecfb
Civitaspo <civitaspo@gmail.com>
2020-03-08 17:47:58 +0900

    Remove delegated methods with delegation macro annotation.

--- src/main/scala/org/embulk/input/dynamodb/PluginTask.scala
+++ src/main/scala/org/embulk/input/dynamodb/PluginTask.scala
@@ -11,7 +11,7 @@ import org.embulk.config.{
   Task,
   TaskSource
 }
-import org.embulk.input.dynamodb.aws.{Aws, HttpProxy}
+import org.embulk.input.dynamodb.aws.Aws
 import org.embulk.input.dynamodb.deprecated.Filter
 import org.embulk.input.dynamodb.item.DynamodbItemSchema
 import org.embulk.input.dynamodb.operation.{
@@ -20,7 +20,7 @@ import org.embulk.input.dynamodb.operation.{
   DynamodbScanOperation
 }
 import org.embulk.spi.BufferAllocator
-import org.embulk.spi.unit.LocalFile
+import zio.macros.annotation.delegate
 
 import scala.util.chaining._
 
@@ -89,105 +89,60 @@ trait PluginTask
   def getBufferAllocator: BufferAllocator
 }
 
-object PluginTask {
+case class PluginTaskCompat(@delegate task: PluginTask) extends PluginTask {
 
-  /*
-   * TODO: Use 'delegate' modifier when Scala 3 is released.
-   *       Or implement '@delegate' annotation with macro.
-   */
-  case class PluginTaskCompat(task: PluginTask) extends PluginTask {
+  override def getOperation: Optional[String] = {
+    task.getOperation.ifPresent { op =>
+      logger.warn(
+        "[Deprecated] The option \"operation\" is deprecated. Use \"scan\" or \"query\" option instead."
+      )
 
-    override def getOperation: Optional[String] = {
-      task.getOperation.ifPresent { op =>
-        logger.warn(
-          "[Deprecated] The option \"operation\" is deprecated. Use \"scan\" or \"query\" option instead."
-        )
-
-        op.toLowerCase match {
-          case "scan" | "query" => // do nothing
-          case x =>
-            throw new ConfigException(
-              s"Operation '$x' is unsupported. Available values are 'scan' or 'query'."
-            )
-        }
-
-        if (getScan.isPresent || getQuery.isPresent)
+      op.toLowerCase match {
+        case "scan" | "query" => // do nothing
+        case x =>
           throw new ConfigException(
-            "The option \"operation\" must not be used together with either \"scan\" or \"query\" options."
+            s"Operation '$x' is unsupported. Available values are 'scan' or 'query'."
           )
       }
-      task.getOperation
+
+      if (getScan.isPresent || getQuery.isPresent)
+        throw new ConfigException(
+          "The option \"operation\" must not be used together with either \"scan\" or \"query\" options."
+        )
     }
-
-    override def getLimit: Long = {
-      logger.warn(
-        "[Deprecated] The option \"limit\" is deprecated. Use \"query.batch_size\" or \"scan.batch_size\" instead."
-      )
-      task.getLimit
-    }
-
-    override def getScanLimit: Long = {
-      logger.warn(
-        "[Deprecated] The option \"scan_limit\" is deprecated. Use \"query.batch_size\" or \"scan.batch_size\" instead."
-      )
-      task.getScanLimit
-    }
-
-    override def getRecordLimit: Long = {
-      logger.warn(
-        "[Deprecated] The option \"record_limit\" is deprecated. Use \"query.limit\" or \"scan.limit\" instead."
-      )
-      task.getRecordLimit
-    }
-
-    override def getFilters: Optional[Filter] = {
-      logger.warn(
-        "[Deprecated] The option \"filters\" is deprecated. Use \"query.filter_expression\" or \"scan.filter_expression\" instead."
-      )
-      task.getFilters
-    }
-
-    override def getTable: String = task.getTable
-
-    override def getColumns: DynamodbItemSchema.SchemaConfigCompat =
-      task.getColumns
-    override def getScan: Optional[DynamodbScanOperation.Task] = task.getScan
-    override def getQuery: Optional[DynamodbQueryOperation.Task] = task.getQuery
-    override def getBufferAllocator: BufferAllocator = task.getBufferAllocator
-    override def getHttpProxy: Optional[HttpProxy.Task] = task.getHttpProxy
-
-    override def getEnableEndpointDiscovery: Optional[Boolean] =
-      task.getEnableEndpointDiscovery
-    override def getAuthMethod: String = task.getAuthMethod
-    override def getAccessKey: Optional[String] = task.getAccessKey
-    override def getAccessKeyId: Optional[String] = task.getAccessKeyId
-    override def getSecretKey: Optional[String] = task.getSecretKey
-    override def getSecretAccessKey: Optional[String] = task.getSecretAccessKey
-    override def getSessionToken: Optional[String] = task.getSessionToken
-    override def getProfileFile: Optional[LocalFile] = task.getProfileFile
-    override def getProfileName: String = task.getProfileName
-    override def getRoleArn: Optional[String] = task.getRoleArn
-    override def getRoleSessionName: Optional[String] = task.getRoleSessionName
-    override def getRoleExternalId: Optional[String] = task.getRoleExternalId
-
-    override def getRoleSessionDurationSeconds: Optional[Int] =
-      task.getRoleSessionDurationSeconds
-    override def getScopeDownPolicy: Optional[String] = task.getScopeDownPolicy
-
-    override def getWebIdentityTokenFile: Optional[String] =
-      task.getWebIdentityTokenFile
-    override def validate(): Unit = task.validate()
-    override def dump(): TaskSource = task.dump()
-    override def getEndPoint: Optional[String] = task.getEndPoint
-    override def getEndpoint: Optional[String] = task.getEndpoint
-    override def getRegion: Optional[String] = task.getRegion
-    override def getDefaultTimeZoneId: String = task.getDefaultTimeZoneId
-
-    override def getDefaultTimestampFormat: String =
-      task.getDefaultTimestampFormat
-    override def getDefaultDate: String = task.getDefaultDate
-    override def getJsonColumnName: String = task.getJsonColumnName
+    task.getOperation
   }
+
+  override def getLimit: Long = {
+    logger.warn(
+      "[Deprecated] The option \"limit\" is deprecated. Use \"query.batch_size\" or \"scan.batch_size\" instead."
+    )
+    task.getLimit
+  }
+
+  override def getScanLimit: Long = {
+    logger.warn(
+      "[Deprecated] The option \"scan_limit\" is deprecated. Use \"query.batch_size\" or \"scan.batch_size\" instead."
+    )
+    task.getScanLimit
+  }
+
+  override def getRecordLimit: Long = {
+    logger.warn(
+      "[Deprecated] The option \"record_limit\" is deprecated. Use \"query.limit\" or \"scan.limit\" instead."
+    )
+    task.getRecordLimit
+  }
+
+  override def getFilters: Optional[Filter] = {
+    logger.warn(
+      "[Deprecated] The option \"filters\" is deprecated. Use \"query.filter_expression\" or \"scan.filter_expression\" instead."
+    )
+    task.getFilters
+  }
+}
+
+object PluginTask {
 
   def load(configSource: ConfigSource): PluginTask = {
     configSource
